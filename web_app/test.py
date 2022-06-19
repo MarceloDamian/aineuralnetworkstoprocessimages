0f00a2f89e0bcca1f3a4a52530478b36ebbbd1f1
Daniel Berger <DanielBergerNY@gmail.com>
2022-06-19 15:24:54 -0400

    Set up flask app and templates

--- web_app/test.py
+++ web_app/test.py
@@ -1,1 +1,12 @@
-print("hello world!")
+# Basic Flask Server
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
