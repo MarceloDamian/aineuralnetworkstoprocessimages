5c5f732037ab642920e053656a9e3f40efa136a9
Daniel Berger <DanielBergerNY@gmail.com>
2022-06-20 15:16:52 -0400

    layers, activation functions, data
    loaded MNIST data set to variables, created an input layer, and started activation functions

--- GAN/neuralnet.py
+++ GAN/neuralnet.py
@@ -5,7 +5,14 @@ import matplotlib.pyplot as plt
 # Using this library to understand pixels/images
 from PIL import Image as im
 
-class Layer():
+# loads the MNIST Dataset
+class MNIST():
+    def __init__(self, filename):
+        self.raw_data = pd.read_csv(filename)
+        self.l = raw_data['label']
+        self.d = raw_data.drop('label', axis = 1)
+
+class InputLayer():
     def __init__(self, num_inputs, num_neurons):
         self.weights = 0.01 * np.random.randn(num_inputs, num_neurons) # initializes weights as a inputs x neruons size array with numbers close to 0
         self.biases = np.zeros((1, num_neurons)) # iniitalizes biases as 0s
@@ -13,22 +20,40 @@ class Layer():
     def feedforward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-class ActivationFunction():
-    pass
+class Softmax():
+    def feed(self, inputs):
+        pass
 
 class LossFunction():
     pass
 
 
 if __name__ == '__main__':
-    test = Layer(3,3)
-    print(test.weights)
-    print("\n")
-    print(test.biases)
-    print("\n")
+    # test = Layer(3,3)
+    # print(test.weights)
+    # print("\n")
+    # print(test.biases)
+    # print("\n")
 
 
     # Data 
 
-    data = pd.read_csv('./train.csv')
+    # data = MNIST('./train.csv')
+    raw_data = pd.read_csv('./train.csv')
+    l = raw_data['label']
+    d = raw_data.drop('label', axis = 1)
+
+    # print(d.head(3).T) # prints the first 5 rows
+
+    # print(l)
+
+    # MNIST is a data set of 28x28 images
+    # Creates a layer with 784 inputs 
+    layer0 = InputLayer(784, 10)
+    layer0.feedforward(d)
+    print(layer0.output.shape)
+
+    df = pd.DataFrame(layer0.output)
+    df.to_csv('layer0output.csv')
+    # print(layer0.output)
 
